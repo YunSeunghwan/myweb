1b8c1b654fdb5e8a755ccd4fd1a25d508755cf18
YunSeunghwan <59220456+YunSeunghwan@users.noreply.github.com>
2021-07-26 23:02:33 +0900

    Update clockJS.js

--- js/clockJS.js
+++ js/clockJS.js
@@ -9,18 +9,17 @@ function setNumPosition() {
 			  nums[i].offsetWidth / 2 + 
 			  132 * Math.abs(Math.sin(angle).toFixed(2));
 		const y = 132 - 132 * Math.abs(Math.cos(angle).toFixed(2));
-		if (i + 1 > 6) nums[i].style.right = '${x}px';
-		else nums[i].style.left = '${x}px';
+		if (i + 1 > 6) nums[i].style.right = `${x}px`;
+		else nums[i].style.left = `${x}px`;
 		
 		if ((i+1 >= 9 && i + 1 <= 12) || (i + 1 >= 1 && i + 1 <= 3))
-			nums[i].style.top = '${y}px';
-		else nums[i].style.bottom = '${y}px';
+			nums[i].style.top = `${y}px`;
+		else nums[i].style.bottom = `${y}px`;
 	}
-	alert("asdf")
 }
 
-function clock() {//쿼리를 불러오는 것 같은데... 쿼리가 없는데??
-	const digital Clock = document.querrySelector(".digital-clock");
+function clock() {//쿼리를 불러오는 것 같은데... 쿼리가 없는데?? 백틱이라 작동이 안됐음.(`')
+	const digitalClock = document.querrySelector(".digital-clock");
 	const secondPointer = document.getElementById("second");
 	const minutePointer = document.getElementById("minute");
 	const hourPointer = document.getElementById("hour");
@@ -29,25 +28,26 @@ function clock() {//쿼리를 불러오는 것 같은데... 쿼리가 없는데?
 	
 	let second = currentTime.getSeconds();
 	let secondAngle = second * 6;
-	let secondAngleValue = 'rotate(${secondAngle}deg)';
+	let secondAngleValue = `rotate(${secondAngle}deg)`;
 	
 	let minute = currentTime.getMinutes();
 	let minuteAngle = minute * 6;
-	let minuteAngleValue = 'rotate(${minuteAngle}deg)';
+	let minuteAngleValue = `rotate(${minuteAngle}deg)`;
 	
 	let hour = currentTime.getHours();
 	let hourAngle = (hour > 12 ? (hour - 12) * 30 : hour * 30) + (minute / 60) * 30;
-	let hourAngleValue = 'rotate(${hourAngle}deg)';
+	let hourAngleValue = `rotate(${hourAngle}deg)`;
 	
 	secondPointer.style.transform = secondAngleValue;
 	minutePointer.style.transform = minuteAngleValue;
 	hourPointer.style.transform = hourAngleValue;
-	digitalClock.innertext = '${
-	hour < 10
-	? '오전 0${hour}'
-	: '${hour > 12 ? '오후 0${hour - 12}' : '오전 ${hour}'}'
-}:${minute < 10 ? '0${minute}' : '${minute}'}:${second < 10? '0${second}' : '${second}'}';
-'
+	digitalClock.innertext = `${
+		hour < 10
+			? `오전 0${hour}`
+			: `${hour > 12 ? `오후 0${hour - 12}` : `오전 ${hour}`}`
+	}:${minute < 10 ? `0${minute}` : `${minute}`}:${
+	second < 10? `0${second}` :	`${second}`
+	}`;
 }
 
 function handleLoad() {
